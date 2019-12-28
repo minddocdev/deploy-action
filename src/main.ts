1c8a2ef53858ae1af1ec704d9de73b11b360b7b9
Darío Blanco <dblancoit@gmail.com>
2019-12-28 18:52:38 +0100

    Show config

--- src/main.ts
+++ src/main.ts
@@ -124,6 +124,7 @@ async function run() {
     await createKubeConfig(kubeConfig);
 
     core.startGroup('Configured cluster information');
+    exec('kubectl config view');
     exec('kubectl cluster-info');
     exec(`kubectl get all -n ${namespace}`);
     core.endGroup();
