b0d7b54b212b08fdfa2cde37da349bcedf7c4f3b
Darío Blanco <dblancoit@gmail.com>
2020-03-02 10:43:52 +0100

    Use version variable in sentry code

--- src/sentry.ts
+++ src/sentry.ts
@@ -2,21 +2,21 @@ import * as core from '@actions/core';
 import { exec } from 'shelljs';
 
 export function setSentryRelease(
-  authToken: string, org: string, appName: string, commitSha: string, environment: string,
+  authToken: string, org: string, appName: string, version: string, environment: string,
 ) {
   core.info(`Set up sentry release for ${environment}`);
   const sentryCliReleases = `sentry-cli --auth-token ${authToken} releases --org ${org}`;
-  const newReleaseCommand = `${sentryCliReleases} new -p ${appName} ${commitSha}`;
+  const newReleaseCommand = `${sentryCliReleases} new -p ${appName} ${version}`;
   core.info(newReleaseCommand);
   if (exec(newReleaseCommand).code !== 0) {
     throw new Error(`Unable to prepare ${appName} sentry release`);
   }
-  const setCommitsCommand = `${sentryCliReleases} set-commits --auto ${commitSha}`;
+  const setCommitsCommand = `${sentryCliReleases} set-commits --auto ${version}`;
   core.info(setCommitsCommand);
   if (exec(setCommitsCommand).code !== 0) {
     throw new Error(`Unable set commits for ${appName} sentry release`);
   }
-  const deployCommand = `${sentryCliReleases} deploys ${commitSha} new -e ${environment}`;
+  const deployCommand = `${sentryCliReleases} deploys ${version} new -e ${environment}`;
   core.info(deployCommand);
   if (exec(deployCommand).code !== 0) {
     throw new Error(`Unable to deploy ${appName} to sentry`);
